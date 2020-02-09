3e85792644307e26ad3e4997606f5a6ea6c2965b
mogere <pmnyatichi@gmail.com>
2020-02-09 14:58:06 +0300

    design main page

--- app/src/main/java/com/mogere/ticketyako/network/TicketMasterApi.java
+++ app/src/main/java/com/mogere/ticketyako/network/TicketMasterApi.java
@@ -1,8 +1,16 @@
 package com.mogere.ticketyako.network;
 
+import com.mogere.ticketyako.models.TicketMasterSearchResponse;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
-public interface TicketMasterApi {
+public interface    TicketMasterApi {
+
+    @GET("events/")
+    Call<TicketMasterSearchResponse> getEvents(
+            @Query("event") String event,
+            @Query("term") String term
+    );
 }
