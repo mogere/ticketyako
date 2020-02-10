cda9828c6cb03a5d21c0179e893d64aabefe8b64
mogere <pmnyatichi@gmail.com>
2020-02-10 08:27:03 +0300

    add code to restaurant list adapter

--- app/src/main/java/com/mogere/ticketyako/adapters/EventListAdapter.java
+++ app/src/main/java/com/mogere/ticketyako/adapters/EventListAdapter.java
@@ -10,13 +10,16 @@ import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.mogere.ticketyako.R;
 import com.mogere.ticketyako.models.TicketMasterSearchResponse;
 
 import java.util.List;
 
-public class EventListAdapter {
+public class EventListAdapter extends RecyclerView.Adapter<EventListAdapter.EventViewHolder>{
 
     public List<TicketMasterSearchResponse> mEvents;
     private Context mContext;
@@ -27,5 +30,47 @@ public class EventListAdapter {
     }
 
 
+    @NonNull
+    @Override
+    public EventListAdapter.EventViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.event_list_item, parent, false);
+        EventViewHolder viewHolder = new EventViewHolder(view);
+        return viewHolder;
+    }
 
+    @Override
+    public void onBindViewHolder(@NonNull EventListAdapter.EventViewHolder holder, int position) {
+        holder.bindEvent(mEvents.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mEvents.size();
+    }
+
+    public class EventViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        @BindView(R.id.eventImageView) ImageView mRestaurantImageView;
+        @BindView(R.id.eventNameTextView) TextView mNameTextView;
+        @BindView(R.id.categoryTextView) TextView mCategoryTextView;
+        @BindView(R.id.ratingTextView) TextView mRatingTextView;
+
+        private Context mcontext;
+
+        public EventViewHolder(View itemView){
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            mContext = itemView.getContext();
+            itemView.setOnClickListener(this);
+        }
+
+
+        @Override
+        public void onClick(View v) {
+
+        }
+
+        public void bindEvent(TicketMasterSearchResponse event){
+
+        }
+    }
 }
